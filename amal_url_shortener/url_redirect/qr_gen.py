76825d47e221f73b3f83036f890114f81df1f581
AMuh2020 <leasu.muhmeen@gmail.com>
2023-02-16 21:26:32 +0000

    need to test from image generate to stringIO

--- amal_url_shortener/url_redirect/qr_gen.py
+++ amal_url_shortener/url_redirect/qr_gen.py
@@ -1,7 +1,7 @@
 # function to generate qr code
 import qrcode
 import os 
-
+from io import StringIO
 def generate_qr(url, short_code):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     print("IN QR GEN")
@@ -10,9 +10,16 @@ def generate_qr(url, short_code):
     qr_file_path = dir_path + "/static/url_redirect/qr_codes/" + str(short_code) + ".png"
     print("2")
     print(qr_file_path)
-    img.save(qr_file_path)
+    imgdata = StringIO()
+    img.save(imgdata,format='svg')
+    imgdata.seek(0)
+    data = imgdata.getvalue()
+    print(data)
     print("3")
     print(f"Created qr code for{short_code}")
+    return data
+    #modified for file object 
+    
 
 def delete_qr(short_code):
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -22,3 +29,5 @@ def delete_qr(short_code):
 
 # FIX STATIC FILE STUFFS FOR DEPLOYMENT
 # CREATE CODE TO CLEAN UP QR CODES AFTER URL OBJECT DELETION
+
+#NEW METHOD CONVERT QR CODE INTO BYTES USING STRING IO, WHEN RENDERED BY HTML IT IS AN IIMAGE
